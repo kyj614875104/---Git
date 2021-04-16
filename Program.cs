8ca44e402a65a1722959e9c8f27ab675a22241fc
kyj1441 <614875104@qq.com>
2021-04-16 13:46:40 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace 测试Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KYJ第一次修改！");
+            Console.WriteLine("ZYY第一次修改");
+        }
+    }
+}
